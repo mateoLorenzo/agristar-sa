2a0fb820da844e82620fb374673073a728d8b994
mateolorenzo <mateolorenzo.dev@gmail.com>
2025-12-30 20:07:50 -0300

    Adjusted mail sending

--- app/api/mail/route.ts
+++ app/api/mail/route.ts
@@ -10,7 +10,7 @@ export async function POST(request: Request) {
   const { data, error } = await resend.emails.send({
     from: "Landing Page <onboarding@resend.dev>",
     // to: "info@agristar.com",
-    to: "matu091201@gmail.com",
+    to: "agristarsa@gmail.com",
     subject: "Nueva consulta de contacto",
     html: await render(
       ContactFormEmail({
